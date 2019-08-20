705a7919105da55ad40d5a53656824c06e1339ab
Brandon U <unzueta.brandon@gmai.com>
2019-08-19 17:38:28 -0700

    changed post function pt-2

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,11 @@
 var friendsData = require("../data/friends");
 
 module.exports = function (app) {
-    app.get("/api/friends", function (err,res) {
+    app.get("/api/friends", function (err, res) {
         res.json(friendsData);
-    })
+    });
+    app.post("/api/tables", function (req, res) {
+        friendsData.push(req.body);
+        res.json(true);
+    });
 }

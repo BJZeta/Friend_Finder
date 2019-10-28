f0ac65f1251fcb5bba10c61f1dd0d0f24e436b2a
Brandon U <unzueta.brandon@gmai.com>
2019-10-27 17:29:13 -0700

    setting up fixes with code

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,8 +5,34 @@ module.exports = function (app) {
         res.json(friendsData);
     });
     app.post("/api/friends", function (req, res) {
-        friendsData.push(req.body);
-        res.json(friendsData);
+        var newUser = req.body;
+
+        for(var i = 0; i < newUser.scores.length; i++) {
+            newUser.scores[i] = parseInt(newUser.scores[i]);
+        }
         
+        var friendIndex = 0;
+
+        var minDiff = 40;
+
+        for(var i = 0; i < friendsData.length; i++) {
+            var totalDiff = 0;
+            for(var i2 = 0; i2 < friendsData[i].scores.length; i2++) {
+                 var difference = Math.abs(newUser.scores[i2] - friendsData[i].scores[i2]);
+                 totalDiff += difference
+            }
+
+            if(totalDiff < minDiff) {
+                friendIndex = i;
+                minDiff = totalDiff;
+            }
+
+        }
+
+        friendsData.push(newUser);
+        
+        res.json(friendsData[friendIndex]);
+
+
     });
 }
